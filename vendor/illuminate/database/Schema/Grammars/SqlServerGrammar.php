33e5611b373a763cfd29c36d61be9284af19e5fb
Frank Warius <frank@warius.de>
2025-10-21 13:07:47 +0200

    serverGrammer patch

--- vendor/illuminate/database/Schema/Grammars/SqlServerGrammar.php
+++ vendor/illuminate/database/Schema/Grammars/SqlServerGrammar.php
@@ -834,7 +834,7 @@ class SqlServerGrammar extends Grammar
             $column->default(new Expression('CURRENT_TIMESTAMP'));
         }
 
-        return $column->precision ? "datetime2($column->precision)" : 'datetime(0)';
+        return $column->precision ? "datetime2($column->precision)" : 'datetime2(0)';
     }
 
     /**
